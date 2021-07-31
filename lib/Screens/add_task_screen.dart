8ef3ca43d659e79c20b3694c327313d65e5ab7c9
johnnyandedi <yetshandedi@gmail>
2021-07-31 07:16:07 +0100

    form key

--- lib/Screens/add_task_screen.dart
+++ lib/Screens/add_task_screen.dart
@@ -11,21 +11,38 @@ class _AddTaskScreenState extends State<AddTaskScreen> {
   final _formkey = GlobalKey<FormState>();
   String _title = "";
   String _priority = "";
-  DateTime _date  = DateTime.now();
-
+  DateTime _date = DateTime.now();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         body: Column(
       children: [
-        GestureDetector(onTap: () {}, child: Icon(Icons.arrow_back_ios))
-      ,
-      SizedBox(height: 20.0),
-      
-      Text("Add Text", style :TextStyle(
-        color: Colors.black , fontSize:40.0 , fontWeight:FontWeight.bold
-      ))
+        GestureDetector(onTap: () {}, child: Icon(Icons.arrow_back_ios)),
+        SizedBox(height: 20.0),
+        Text("Add Text",
+            style: TextStyle(
+                color: Colors.black,
+                fontSize: 40.0,
+                fontWeight: FontWeight.bold)),
+        SizedBox(height: 10),
+        Form(
+            key: _formkey,
+            child: Column(
+              children: [
+                TextFormField(
+                  decoration: InputDecoration(
+                    labelText: 'title', 
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10.0)
+                    )
+                  ),
+                  style: TextStyle(
+                    fontSize: 10.0
+                  ),
+                )
+              ],
+            ))
       ],
     ));
   }
